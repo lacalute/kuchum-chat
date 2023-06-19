476661ae72a4b0b72fce716ca85dca0e0dd5dba0
lacalute <123@gmail.com>
2023-06-19 17:30:18 +0300

    fix jwt token

--- backend/components/auth.py
+++ backend/components/auth.py
@@ -10,13 +10,13 @@ def login(user: User, req: Request, res: Response):
   user_db = chat_users.find_one({'nick': user.nick})
 
   if user_db and check_hashing(str(user.password), user_db['password']):
-    res.set_cookie(key='access_token_cookie', value=token.create_access_token(user_db['_id']), samesite="none", secure=True)
+    res.set_cookie(key='access_token_cookie', value=token.create_access_token(user_db['_id']), samesite="none", secure=True, max_age=120, expires=120)
     return {"msg": "Successfuly login"}
 
   elif not user_db:
     crud_security.create(user_payload(user.nick, user.password))
     user_db = chat_users.find_one({'nick': user.nick})
-    res.set_cookie(key='access_token_cookie', value=token.create_access_token(user_db['_id']), samesite="none", secure=True)
+    res.set_cookie(key='access_token_cookie', value=token.create_access_token(user_db['_id']), samesite="none", secure=True, max_age=120, expires=120)
     return {"msg": "Successfuly register"}
 
   return {'msg': 'Creditionals are bad'}
