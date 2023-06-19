647f1f2cd2cd83a976c89eb6d966f19d9436b443
lacalute <123@gmail.com>
2023-06-19 22:55:08 +0300

    auth page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Auth } from './components/Auth/auth';
-import { ReactComponent as Logo } from './title.png';
+
 
 function App() {
   return (
     <>
     <div className='container'>
     <img src={require('./title.png')} className='title_img' />
+      <h1 className='title_text'>Вход в Kuchat</h1>
       <Auth />
     </div>
     </>
--- frontend/src/components/Auth/auth.js
+++ frontend/src/components/Auth/auth.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
+import './auth.css'
 
 export const Auth = () => {
   const navigate = useNavigate();
@@ -21,18 +22,17 @@ export const Auth = () => {
 
   }
   
-  if (isLoading) return <h1>Загрузка...</h1>
+  if (isLoading) return <h1>Вход в систему...</h1>
 
 
   return (
     <>
-    <h1>Вход в мессенджер</h1>
     <form method="post" onSubmit={submitHandler}>
-      <input required value={values.nick} onChange={e => setValues({ ...values, nick: e.target.value })} id='title' type='text' placeholder='Your Nick' />
-      <input required value={values.password} onChange={e => setValues({ ...values, password: e.target.value })} id='title' type='text' placeholder='Your Password' />
+      <input className='input' required value={values.nick} onChange={e => setValues({ ...values, nick: e.target.value })} id='title' type='text' placeholder='Your Nick' />
+      <input className='input' required value={values.password} onChange={e => setValues({ ...values, password: e.target.value })} id='title' type='text' placeholder='Your Password' />
       <button className='post_submit' type='submit'>Войти</button>
     </form>
-    <h4>Если у вас нет аккаунта, то создайте его прям тут же</h4>
+    <h4 className='info'>Если у вас нет аккаунта, то создайте его прям тут же</h4>
     </>
   )
 
