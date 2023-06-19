9c05c334f7eb4a944107fb1ecca219e898c90bee
lacalute <123@gmail.com>
2023-06-19 21:18:57 +0300

    fu

--- backend/config.py
+++ backend/config.py
@@ -26,36 +26,30 @@ from typing import Callable, List
 from fastapi.routing import APIRoute
 
 
+
+app = FastAPI()
+
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "https://kuch-chat.vercel.app"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
 origins = [
   "https://kuch-chat.vercel.app"
 ]
 
-middleware = [
-  Middleware(CORSMiddleware, allow_origins=origins)
-]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app = FastAPI(middleware=middleware)
-
-# Salt to your taste
-ALLOWED_ORIGINS = 'https://kuch-chat.vercel.app'    # or 'foo.com', etc.
-
-# handle CORS preflight requests
-@app.options('/{rest_of_path:path}')
-async def preflight_handler(request: Request, rest_of_path: str) -> Response:
-    response = Response()
-    response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
-    return response
-
-# set CORS headers
-@app.middleware("http")
-async def add_CORS_header(request: Request, call_next):
-    response = await call_next(request)
-    response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
-    return response
 
 
 class CRUD:
