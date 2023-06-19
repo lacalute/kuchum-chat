b23bb432d2bd9d7790f65e7f031daae6d1eb3c1c
lacalute <123@gmail.com>
2023-06-19 15:43:02 +0300

    fix message

--- backend/components/talk.py
+++ backend/components/talk.py
@@ -4,10 +4,10 @@ from tokens import *
 crud_chat = CRUD(chat_relate)
 
 @app.post('/api/message', tags=['message'])
-def create_message(msg: Message, chat_id, req: Request, auth: AuthJWT = Depends()):
-  TOKEN = token.tokens_required(req, 2, auth)
+def create_message(msg: Message, chat_id, res: Response, req: Request):
+  TOKEN = token.token_required(res, req, 2)
   chat_history = crud_chat.get_id(chat_id)['history']
-  chat_history.append(message_scheme(TOKEN['access']['user_id'], msg.message))
+  chat_history.append(message_scheme(TOKEN['user_id'], msg.message))
   crud_chat.update(chat_id, {'history': chat_history})
   return {'msg': 'Message sent'}
 
