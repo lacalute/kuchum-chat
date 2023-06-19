913edc942211afd3931060d91f4d3305736f6e8a
lacalute <123@gmail.com>
2023-06-19 12:50:07 +0300

    refactor: fix token jwt

--- backend/components/auth.py
+++ backend/components/auth.py
@@ -10,13 +10,13 @@ def login(user: User, req: Request, res: Response, Authorize: AuthJWT = Depends(
   user_db = chat_users.find_one({'nick': user.nick})
 
   if user_db and check_hashing(str(user.password), user_db['password']):
-    res.set_cookie('access_token_cookie', token.create_access_token(user_db['_id'], samesite="none", secure=True))
+    res.set_cookie(token='access_token_cookie', value=token.create_access_token(user_db['_id']), samesite="none", secure=True)
     return {"msg": "Successfuly login"}
 
   elif not user_db:
     crud_security.create(user_payload(user.nick, user.password))
     user_db = chat_users.find_one({'nick': user.nick})
-    res.set_cookie('access_token_cookie', token.create_access_token(user_db['_id'], samesite="none", secure=True))
+    res.set_cookie(token='access_token_cookie', value=token.create_access_token(user_db['_id']), samesite="none", secure=True)
     return {"msg": "Successfuly register"}
 
   return {'msg': 'Creditionals are bad'}
