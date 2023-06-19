f214cbd7be5baf14670afb049d2bff63cd9218d6
lacalute <123@gmail.com>
2023-06-19 12:11:19 +0300

    fix hash shit

--- backend/hashing.py
+++ backend/hashing.py
@@ -3,5 +3,5 @@ import bcrypt
 def hashing(plain_text_password):
   return bcrypt.hashpw(plain_text_password, bcrypt.gensalt())
 
-def check_hashing(plain_text_password, hashed_password):
-  return bcrypt.checkpw(plain_text_password, hashed_password)
+def check_hashing(plain_text_password: str, hashed_password):
+  return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)
