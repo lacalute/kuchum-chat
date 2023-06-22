ae4f0d2d94dc163bd2ac490267d9e8d58f2f9fbc
lacalute <123@gmail.com>
2023-06-22 18:49:53 +0300

    update

--- frontend/src/components/Message/message.js
+++ frontend/src/components/Message/message.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
 import {useParams} from "react-router-dom";
 
-
-
-
 export function Message() {
   const {chat_id} = useParams();
-  const [data, setData] = useState()
+  const [userId, setUserId] = useState()
   const [msg, setMsg] = useState()
   const [values, setValues] = useState({message: ''})
 
@@ -16,14 +13,21 @@ export function Message() {
     fetch('https://chat-backend-86jx.onrender.com/api/profile', {'credentials': 'include'})
       .then(res => res.json())
       .then(data => {
-        setData(data)
+        setUserId(data)
+      })
+  }, [])
+  useEffect(() => {
+    fetch('https://chat-backend-86jx.onrender.com/api/chat', {'credentials': 'include', 'method': 'GET'})
+      .then(res => res.json())
+      .then(data => {
+        setMsg(data)
       })
   }, [])
 
   const submitHandler = e => {
     e.preventDefault()
-    if (data != undefined && data != 'null'){
-      socket.send(JSON.stringify({'msg': values.message, 'id': data.user_id}))
+    if (userId != 'null') {
+      socket.send(JSON.stringify({'msg': values.message, 'id': userId.user_id}))
     }
     setValues({message: ''})
   }
@@ -53,15 +57,6 @@ export function Message() {
     </>
   )
 
-  return (
-    <>
-    <form method="post" onSubmit={submitHandler}>
-    <input value={values.message} onChange={e => setValues({ ...values, message: e.target.value })} placeholder="Написать сообщение"></input>
-    <button type="submit">отправить</button>
-    </form>
-    <a href='/'>Выйти из чата</a>
-    </>
-  )
 }
 
 
